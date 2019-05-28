8adf9eb95c3e9cba6f62c0ccbb9988beece93e5d
YJUNY <36507979+YOUNGJUN2@users.noreply.github.com>
2019-05-28 22:42:05 +0900

    Add files via upload

---
+++ lotto 자동번호 추출/lottoGame.py
@@ -0,0 +1,23 @@
+import random
+
+def lotto_game(n):
+    ln = []
+    
+    for i in range(n):
+        lr=[]
+        while len(lr)!=6:
+            a = random.randint(1,45)
+            breaker = False
+            for k in lr:
+                if k == a:
+                    breaker = True
+            if breaker == True:
+                continue
+            lr.append(a)
+        lr.sort()
+        ln.append(lr)
+        
+        print(ln[i])
+        
+n = int(input("게임 수를 입력하세요: "))
+lotto_game(n)
