15f42c1fc8e73db3edb6e417cdced721e18bc7b3
YJUNY <36507979+YOUNGJUN2@users.noreply.github.com>
2019-07-01 21:16:17 +0900

    Update not_Player.py

--- 프로그래머스/not_Player.py
+++ 프로그래머스/not_Player.py
@@ -1,22 +1,15 @@
-import collections
+from collections import Counter
 
 def solution(participant, completion):
     answer = ''
 
-    l = list(set(participant) - set(completion))    
+    a = Counter(participant)
+    b = Counter(completion)
+
+    answer = a - b
+    answer = list(answer.keys())
+    answer = answer.pop()
     
-    if not l:
-        a = collections.Counter(participant)
-        b = collections.Counter(completion)
-        
-        c = a-b
-        c = list(c.keys())
-        
-        answer = c.pop()   
-        
-    else:
-        answer = l.pop()
-        
     return answer
 
 # 문제: https://programmers.co.kr/learn/courses/30/lessons/42576
