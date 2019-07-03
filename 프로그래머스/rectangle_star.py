1d1c74909821bfe1f7b22781541180e0eb008547
YJUNY <36507979+YOUNGJUN2@users.noreply.github.com>
2019-07-03 15:46:36 +0900

    Create rectangle_star.py

---
+++ 프로그래머스/rectangle_star.py
@@ -0,0 +1,8 @@
+a, b = map(int, input().strip().split(' '))
+
+for i in range(b):
+    for j in range(a):
+        print("*", end="")
+    print("")
+    
+# 문제: https://programmers.co.kr/learn/courses/30/lessons/12969
